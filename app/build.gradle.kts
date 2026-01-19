e2f06b9dd29b8ef08c583bbd6ed1fe640743a19e
yashgarg28 <66386881+yashgarg28@users.noreply.github.com>
2026-01-19 12:41:26 +0530

    Changed the name to BrnBook and version to 2 and code to 6.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.example.tempcontacts"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.3.2"
+        versionCode = 6
+        versionName = "2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
